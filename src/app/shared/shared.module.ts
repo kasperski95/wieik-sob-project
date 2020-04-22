c7139c69ea70ef1bd87c149314f675acc9bbd63c
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2020-04-22 22:57:04 +0200

    reduce complexity

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
-import { TranslateModule } from "@ngx-translate/core";
-
-import { PageNotFoundComponent, NodeComponent } from "./components/";
-import { WebviewDirective } from "./directives/";
+import { NodeComponent } from "./components/";
 import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [PageNotFoundComponent, WebviewDirective, NodeComponent],
-  imports: [CommonModule, TranslateModule, FormsModule],
-  exports: [TranslateModule, WebviewDirective, FormsModule, NodeComponent],
+  declarations: [NodeComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [FormsModule, NodeComponent],
 })
 export class SharedModule {}
